3d295f9e09009a9a19aa5fea0a586bb52eae1f95
munozgit85 <107218022+munozgit85@users.noreply.github.com>
2022-08-28 18:32:53 -0500

    display of music list to page

--- assets/script2.js
+++ assets/script2.js
@@ -1,32 +1,39 @@
 var userFormEl = document.querySelector("#form");
-var nameInputEl = document.getElementById("#submitBtn");
+var nameInputEl = document.querySelector("#submitBtn");
 var repoContainerEl = document.querySelector("#list-container");
 var repoSearchTerm = document.querySelector("#music-search-term");
-var clearSearch = document.getElementById("clearBtn");
+var clearSearch = document.querySelector("#clearBtn");
+var userInputEl = document.querySelector("#input-data");
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
   event.preventDefault();
 
   // get value from input element
-  var username = nameInputEl.value.trim();
+  var username = userInputEl.value.trim();
 
   if (username) {
     getUserRepos(username);
 
     // clear old content
     repoContainerEl.textContent = "";
-    nameInputEl.value = "";
+    userInputEl.value = "";
   } else {
     alert("Please valid artist/band name");
   }
 };
 
+function artistName(event) {
+event.preventDefault();
+var userInputEl = document.querySelector("input[name='artist']").value;
+console.log(userInputEl);
+}
 
-var getUserRepos = function(artists) {
-  // format the github api url
- 
-  var apiUrl = " https://api.mixcloud.com/search/?q=" + nameInputEl + "&type=cloudcast";
+
+var getUserRepos = function(data) {
+  // format the api url
+  
+    var apiUrl = "https://api.mixcloud.com/search/?q=" + data + "&type=cloudcast";
  
 
   // make a get request to url
@@ -37,7 +44,7 @@ var getUserRepos = function(artists) {
         console.log(response);
         response.json().then(function(data) {
           console.log(data);
-          displayRepos(data,artists);
+          displayRepos(data.data);
         });
       } else {
         alert('Artist Not Found');
@@ -48,19 +55,21 @@ var getUserRepos = function(artists) {
     });
 };
 
-var displayRepos = function(tracks, SearchTerm) {
+var displayRepos = function(data) {
     // check if api returned any repos
-    if (tracks.length === 0) {
+
+    if (data.length === 0) {
       repoContainerEl.textContent = "No playlist found.";
       return;
     }
   
-    repoSearchTerm.textContent = SearchTerm;
+    //repoSearchTerm.textContent = SearchTerm;
   
     // loop over repos
-    for (var i = 0; i < tracks.length; i++) {
+    for (var i = 0; i < data.length; i++) {
       // format repo name
-      var repoName = data[i].url + "/" + data[i].name;
+
+      var repoName = data[i].name + "/" + data[i].url;
 
 
     // create a link for each repo
@@ -75,23 +84,13 @@ var displayRepos = function(tracks, SearchTerm) {
     // append to container
     repoEl.appendChild(titleEl);
 
-    // create a status element
-    var statusEl = document.createElement("span");
-    statusEl.classList = "flex-row align-center";
 
-
-    // append to container
-    repoEl.appendChild(statusEl);
 
     // append container to the dom
     repoContainerEl.appendChild(repoEl);
   }
 };
-
-var resetForm = function () {
-  location.reload()
-}
-
   // add event listeners to form and button container//
-  userFormEl.addEventListener("submit", formSubmitHandler);
-  clearSearch.addEventListener("click", resetForm);
+  userFormEl.addEventListener("click", artistName);
+  //clearSearch.addEventListener("click", resetForm);
+  nameInputEl.addEventListener("click", formSubmitHandler);
