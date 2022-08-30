e027f5fcc698a8ea70bad1f2294484a29317ca1d
PivotalSaint <licensedmt@att.net>
2022-08-29 20:05:09 -0500

    saving to pull new code

--- assets/script.js
+++ assets/script.js
@@ -94,8 +94,91 @@ var resetForm = function (){
     location.reload()
 }
 
+var formTopFive = function(event) {
+  // prevent page from refreshing
+  event.preventDefault();
+
+  // get value from input element
+  var username = userInputEl.value.trim();
+
+  if (username) {
+    getUserTopFive(username);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+    userInputEl.value = "";
+  } else {
+    alert("Please valid artist/band name");
+  }
+};
+
+var getUserTopFive = function(data) {
+  // format the api url
+  
+    var napUrl = "https://api.napster.com/v2.2/artists/top?limit=5&offset=5&apikey=MWVlYWFlNDQtMzc5NS00M2U3LWI3MTktNTUxMzU3OGY1N2E1";
+ 
+
+  // make a get request to url
+  fetch(napUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function(data) {
+          console.log(data);
+          displayNaps(data.data);
+        });
+      } else {
+        alert('Artist Not Found');
+      }
+    })
+    .catch(function(error) {
+      alert("Unable to connect");
+    });
+};
+
+var displayNaps = function(data) {
+    // check if api returned any repos
+
+    if (data.length === 0) {
+      repoContainerEl.textContent = "No playlist found.";
+      return;
+    }
+  
+    //repoSearchTerm.textContent = SearchTerm;
+  
+    // loop over repos
+    for (var i = 0; i < data.length; i++) {
+      // format repo name
+
+      var repoName = data[i].name + "/" + data[i].url;
+
+
+    // create a link for each repo
+    var repoEl = document.createElement("a");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+
+    // create a span element to hold repository name
+    var titleEl = document.createElement("li");
+    titleEl.textContent = repoName;
+
+    // append to container
+    repoEl.appendChild(titleEl);
+
+
+
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+  }
+};
+var resetForm = function (){
+    location.reload()
+}
+
   // add event listeners to form and button container//
   userFormEl.addEventListener("click", artistName);
   clearSearch.addEventListener("click", resetForm);
   nameInputEl.addEventListener("click", formSubmitHandler);
+  nameInputEl.addEventListener("click", formTopFive);
 
