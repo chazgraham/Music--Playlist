d076ed7f3d055964cfeea149d72a5fb5cc9e62ad
chaz Graham <chazhg80.cg@gmail.com>
2023-09-21 18:36:27 -0500

    top 15 now mirrors the searched query and style, removed the saved songs array due to lack of functonality

--- assets/script.js
+++ assets/script.js
@@ -94,22 +94,22 @@ var displayRepos = function (data) {
     // append container to the dom
     repoContainerEl.appendChild(titleEl);
     
-    saveMusic(repoName)
+    //saveMusic(repoName)
   }
 };
 
-var getNapRepos = function (tracks) {
+var getNapRepos = function (data) {
   // format the github api url
-  var napUrl = "https://api.napster.com/v2.2/artists/Art.28463069/tracks/top?limit=15&offset=15&apikey=MWVlYWFlNDQtMzc5NS00M2U3LWI3MTktNTUxMzU3OGY1N2E1";
+  var napUrl = "https://api.mixcloud.com/search/?q=drake&limit=15&type=cloudcast&apikey=MWVlYWFlNDQtMzc5NS00M2U3LWI3MTktNTUxMzU3OGY1N2E1";
   // make a get request to url
   fetch(napUrl)
     .then(function (response) {
       // request was successful
       if (response.ok) {
         console.log(response);
-        response.json().then(function (tracks) {
-          console.log(tracks);
-          displayTracks(tracks.tracks);
+        response.json().then(function (data) {
+          console.log(data);
+          displayTracks(data.data);
         });
       } else {
         alert('Artist Not Found');
@@ -119,30 +119,43 @@ var getNapRepos = function (tracks) {
       alert("Unable to connect");
     });
 };
-var displayTracks = function (tracks) {
+var displayTracks = function (data) {
+  var title = document.createElement("h2")
+  title.classList = "h2"
+  title.textContent = "Top 15 By Drake"
+  repoContainerEl.appendChild(title)
  
   // loop over repos
-  for (var i = 0; i < tracks.length; i++) {
+  for (var i = 0; i < data.length; i++) {
     // format repo name
-    var napRepo = tracks[i].shortcut;
+    var napRepo = data[i].url;
+    var napRepoName = data[i].name
+    var repoImage = data[i].pictures.large
     // create a link for each repo
     var topFiveEl = document.createElement("a");
-    topFiveEl.classList = "list-item flex-row justify-space-between align-center";
-    topFiveEl.setAttribute("shortcut", napRepo);
+    topFiveEl.textContent = napRepoName
+    topFiveEl.classList = "list-item flex-row justify-space-between align-center overlay";
+    topFiveEl.setAttribute("href", napRepo);
+    topFive.setAttribute("target", "_blank");
+
+    var repoImg = document.createElement("img")
+    repoImg.textContent = repoImage
+    repoImg.setAttribute("src", repoImage)
+
+    var title = document.createElement("h2")
+    title.textContent = "Top 15 By drake"
     
     // create a span element to hold repository name
-
-    var napTitleEl = document.createElement("span");
+    var napTitleEl = document.createElement("div");
+    napTitleEl.classList = 'thumbnail'
     repoContainerEl.classList = "box is-vertical is-size-12 mr-6 ml-6"
 
-    var napTitleEl = document.createElement("li");
-    napTitleEl.textContent = napRepo;
     // append to container
-    topFiveEl.appendChild(napTitleEl);
+    napTitleEl.appendChild(topFiveEl);
+    napTitleEl.appendChild(repoImg);
     
     // append to container
-    topFiveEl.appendChild(napTitleEl);
-    repoContainerEl.appendChild(topFiveEl);
+    repoContainerEl.appendChild(napTitleEl);
   };
   console.log(napRepo);
 };
@@ -151,42 +164,42 @@ var resetForm = function () {
   location.reload()
 }
 
-function saveMusic(data)
-{
-    var playlist = [];
+//function saveMusic(data)
+//{
+    //var playlist = [];
     // Parse the serialized data back into an aray of objects
-    playlist = JSON.parse(localStorage.getItem('playlist')) || [];
+    //playlist = JSON.parse(localStorage.getItem('playlist')) || [];
     // Push the new data (whether it be an object or anything else) onto the array
-    playlist.push(data);
+    //playlist.push(data);
     // Re-serialize the array back into a string and store it in localStorage
-    localStorage.setItem('playlist', JSON.stringify(playlist));
-}
+    //localStorage.setItem('playlist', JSON.stringify(playlist));
+//}
 
-var loadUserSong = function() {
-  var savedUserSong = localStorage.getItem("playlist");
+//var loadUserSong = function() {
+  //var savedUserSong = localStorage.getItem("playlist");
 
   // parse into array of objects
-  songs = JSON.parse(savedUserSong);
-  console.log(songs);
+  //songs = JSON.parse(savedUserSong);
+  //console.log(songs);
 
-  var songs = document.createElement("a");
-  songs.setAttribute("href", savedUserSong);
-  songs.setAttribute("target", "_blank")
+  //var songs = document.createElement("a");
+  //songs.setAttribute("href", savedUserSong);
+  //songs.setAttribute("target", "_blank")
 
-  var savedSong = document.createElement("li");
-  savedSong.className = "box is-vertical is-size-12 mr-6 ml-6";
+  //var savedSong = document.createElement("li");
+  //savedSong.className = "box is-vertical is-size-12 mr-6 ml-6";
   
-  var songData = document.createElement("li");
-  songData.className = "playlist";
-  songData.innerText = songs
+  //var songData = document.createElement("li");
+  //songData.className = "playlist";
+  //songData.innerText = songs
       
-  savedSong.appendChild(songData);
-  repoContainerEl.appendChild(savedSong);  
-};
+  //savedSong.appendChild(songData);
+  //repoContainerEl.appendChild(savedSong);  
+//};
 
 // add event listeners to form and button container//
 userFormEl.addEventListener("click", artistName);
 clearSearch.addEventListener("click", resetForm);
 nameInputEl.addEventListener("click", formSubmitHandler);
-searchedSongList.addEventListener("click", loadUserSong);
+//searchedSongList.addEventListener("click", loadUserSong);
 topFive.addEventListener("click", getNapRepos)
